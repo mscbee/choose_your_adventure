74225ecb2cf3fc84a4598ee5a63a6b97f221839e
Charlene <charlenebadibanga@gmail.com>
2017-12-27 17:59:32 +0000

    added a class for characters

---
+++ src/evermore/Characters.java
@@ -0,0 +1,7 @@
+package evermore;
+
+/**
+ * Created by cba50 on 27/12/2017.
+ */
+public class Characters {
+}
--- src/evermore/Game.java
+++ src/evermore/Game.java
@@ -14,21 +14,25 @@ public class Game {
 
     // TODO: Sort out the logic! At the moment, the do/while block is working as it should
     // but the if statement checking against the polarAnswer is crashing the programme!
-    
-    do {
 
-      System.out.println("Please only respond with 'yes' or 'no'");
-      System.out.println("Are you ready to go on an adventure?");
-      polarAnswer = in.next();
-
-    }
-      while (!polarAnswer.equalsIgnoreCase("yes") || !polarAnswer.equalsIgnoreCase("no"));
 
     if(polarAnswer.equalsIgnoreCase("yes")) {
       System.out.println("Great, " + playerName + "! Let's get started!");
     }
     else if(polarAnswer.equalsIgnoreCase("no")) {
       System.out.println("Okay, " + playerName + ". Maybe we can go another time!\nSee you later!");
+      break;
+    }
+    else {
+        while (!polarAnswer.equalsIgnoreCase("yes") || !polarAnswer.equalsIgnoreCase("no"))
+        {
+
+        System.out.println("Please only respond with 'yes' or 'no'");
+        System.out.println("Are you ready to go on an adventure?");
+
+        // polarAnswer = in.next();
+
+      }
     }
 
   }
